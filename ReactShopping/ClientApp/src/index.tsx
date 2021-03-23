658d0e4d0b43be41c3f817d77b2355cb5d620c8e
Nyakas Péter <thenyakaspeter@gmail.com>
2021-03-23 18:40:20 +0100

    persist

--- ReactShopping/ClientApp/src/index.tsx
+++ ReactShopping/ClientApp/src/index.tsx
@@ -5,14 +5,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Routes } from "./components";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 require("jquery/dist/jquery.min.js");
 require("bootstrap/dist/js/bootstrap.min.js");
 
 ReactDOM.render(
   <Provider store={store}>
-    <Routes />
+    <PersistGate persistor={persistor}>
+      <Routes />
+    </PersistGate>
   </Provider>,
   document.getElementById("app")
 );
--- ReactShopping/ClientApp/src/redux/store.ts
+++ ReactShopping/ClientApp/src/redux/store.ts
@@ -3,15 +3,41 @@ import { users } from "./users/reducer";
 import { todos } from "./todos/reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer, createTransform } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// Transform dates back into JS Dates on rehydrate
+// (see: https://github.com/rt2zz/redux-persist/issues/82)
+const replacer = (key, value) =>
+  value instanceof Date ? value.toISOString() : value;
+const reviver = (key, value) =>
+  typeof value === "string" &&
+  value.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/)
+    ? new Date(value)
+    : value;
+export const encode = toDehydrate => JSON.stringify(toDehydrate, replacer);
+export const decode = toRehydrate => JSON.parse(toRehydrate, reviver);
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [createTransform(encode, decode)]
+};
 
 const rootReducer = combineReducers({
   users,
   todos
 });
 
-export default createStore(
-  rootReducer,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
+export const persistor = persistStore(store);
+
 export type AppState = ReturnType<typeof rootReducer>;
+
+export default { store, persistor };
